94879bbc81b8af8575412a7381aab4720a6fb10e
Brandon Calloway <brandon@scullytown.com>
2010-07-27 09:10:05 -0400

    initial commit

---
+++ overview_dashboard_extension.rb
@@ -0,0 +1,9 @@
+class OverviewDashboardExtension < Spree::Extension
+  version "1.0"
+  description "Fix for the Spree Core dashboard extension to display basic graphs and charts on store activity."
+  url "http://github.com/bcalloway/spree-dashboard"
+
+  def activate
+
+  end
+end
